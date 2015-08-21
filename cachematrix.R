8f68e18a7656e6a355c06ba7828fe44d09d6b008
rpmendoza <rpmendoza@gmail.com>
2015-08-21 10:22:57 -0400

    created 2 functions
    created 2 functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-  print ("this is x!")
-
+  #print ("this is x!")
+  invValue <- NULL
+  set <- function(y) {
+    x <<- y
+    invValue <<- NULL
+  }
+  get <- function() x
+  setInv <- function(solve) invValue <<- solve
+  getInv <- function() invValue
+  list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  invValue <- x$getInv()
+  if (!is.null(invValue)) {
+    return(invValue)
+  }
+  data <- x$get()
+  invValue <- solve(x)
+  x$setInv(invValue)
+  invValue
 }
